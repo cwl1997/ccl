66b69f8e12842a1f0cbd5229299ef4a4b6a74846
cwl1997 <928024702@qq.com>
2020-07-15 15:35:32 +0800

    新增首页

---
+++ src/view/home/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        disabled: false,
+    }
+},
+    watch () {
+
+},
+    computed () {
+
+},
+    created () {
+
+},
+    methods: {
+
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
